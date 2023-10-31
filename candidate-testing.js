23fab059e1e4f16c4d616165cdffbe84bf40fc0a
Your Name <108307350+pjduri@users.noreply.github.com>
2023-10-31 09:45:21 -0500

    testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -20,7 +20,7 @@ function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question('Welcome Candidate!  \nPlease enter your name: ')
 }
-
+// testing
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   candidateAnswer = input.question(question)
